e4bc7f8b76f5de123a08e490dd007f6acee81155
root <ishaqshamunu1@gmail.com>
2023-12-19 18:23:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - was the function
+ * Return: 0 (sucess)
+ */
+
+void print_alphabet(void)
+{
+
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
