ac8763861684aee8a1188739cb738a0351289649
root <ishaqshamunu1@gmail.com>
2023-12-21 16:28:00 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check the code and findout if the letter is upper
+ * @x: Is the number to be checked
+ * Return: 1 for upper 0 if not 
+ */
+
+int _isupper(int x);
+{
+	if (x >= 65 x &&  <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
